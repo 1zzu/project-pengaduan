71499525b109fb402b544ec2ca618a96836d892a
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-04-29 10:42:09 +0700

    Add upload foto

--- pengaduan/add.php
+++ pengaduan/add.php
@@ -4,7 +4,20 @@ include "../connect.php";
 $nik            = isset($_POST["nik"]) ? $_POST["nik"] : "";
 $tglPengaduan   = isset($_POST["tglPengaduan"]) ? $_POST["tglPengaduan"] : "";
 $isiLaporan     = isset($_POST["isiLaporan"]) ? $_POST["isiLaporan"] : "";
-$foto           = isset($_POST["foto"]) ? $_POST["foto"] : "";
+
+$baseUrl        = "http://localhost/project-pengaduan/pengaduan/";
+$target_dir     = "uploads/";
+$target_file    = $target_dir . basename($_FILES["foto"]["name"]);
+
+$imageFileType  = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$check          = getimagesize($_FILES["foto"]["tmp_name"]);
+
+if($check !== false) {
+  $foto         = $baseUrl . $target_file;
+  move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)
+} else {
+  $foto         = '';
+}
 
 $status         = 'baru';
 
