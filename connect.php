491aeb12b7d679910df8dd526540647ec6203a05
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-04-29 09:52:37 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,14 @@
 <?php
-	//diisi dengan hosnya username, password, dan nama database
-	// $conn = mysqli_connect("localhost", "root", "", "pengaduan");
+    //diisi dengan hosnya username, password, dan nama database
+    // $conn = mysqli_connect("localhost", "root", "", "pengaduan");
+
     $servername = "localhost";
     $username = "root";
     $password = "";
     $databasename = "pengaduan";
+
+    // koneksi versiku OOP
+    $db = new mysqli($servername, $username, $password, $databasename);
 
     $conn = mysqli_connect($servername, $username, $password, $databasename);
     if(!$conn){
